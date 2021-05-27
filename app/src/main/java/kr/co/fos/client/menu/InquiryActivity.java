0682540977ee644b3de893451fd029219c703dba
suhyung0812 <suhyung1006@naver.com>
2021-05-27 21:26:49 +0900

    05-27 21:26

---
+++ app/src/main/java/kr/co/fos/client/menu/InquiryActivity.java
@@ -0,0 +1,31 @@
+package kr.co.fos.client.menu;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import kr.co.fos.client.R;
+
+public class InquiryActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_inquiry);
+    }
+
+    //즐겨찾기
+    public void bookmark(){
+
+    }
+
+    //로그아웃
+    public void logout(){
+
+    }
+
+    //메뉴 조회
+    public void menuInquiry(){
+
+    }
+}
+
---
+++ app/src/main/java/kr/co/fos/client/order/BusinessDetailInquiryActivity.java
@@ -0,0 +1,25 @@
+package kr.co.fos.client.order;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import kr.co.fos.client.R;
+
+public class BusinessDetailInquiryActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.order_busienss_detail_inquiry);
+    }
+
+    //주문 상세 조회
+    public void orderDetailInquiry(){
+
+    }
+
+    //주문 상태 변경
+    public void orderStatusUpdate(){
+
+    }
+}
