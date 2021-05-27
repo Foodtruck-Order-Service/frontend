b38bbc70e2ef592c5bd7d7570b448a3c96a3cee6
wkemrm <wkemrm12@naver.com>
2021-05-27 20:34:47 +0900

    no message

--- app/src/main/java/kr/co/fos/client/MainActivity.java
+++ app/src/main/java/kr/co/fos/client/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    //asda
+    //이동연 test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
