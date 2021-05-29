3765576a4a5d91ea845502467145936fd3023663
wkemrm <wkemrm12@naver.com>
2021-05-29 22:41:35 +0900

    메인, 로그인 구현 중

--- app/src/main/java/kr/co/fos/client/common/LoginActivity.java
+++ app/src/main/java/kr/co/fos/client/common/LoginActivity.java
@@ -1,21 +1,137 @@
 package kr.co.fos.client.common;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import kr.co.fos.client.R;
+import com.google.gson.Gson;
 
-public class LoginActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.util.List;
+
+import kr.co.fos.client.HttpInterface;
+import kr.co.fos.client.R;
+import kr.co.fos.client.SharedPreference;
+import kr.co.fos.client.foodtruck.LocationActivity;
+import kr.co.fos.client.foodtruck.SearchResultActivity;
+import kr.co.fos.client.member.FindId1Activity;
+import kr.co.fos.client.member.FindPw1Activity;
+import kr.co.fos.client.member.JoinActivity;
+import kr.co.fos.client.member.JoinChoiceActivity;
+import kr.co.fos.client.member.Member;
+import kr.co.fos.client.member.MyInfoActivity;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    Retrofit client;
+    HttpInterface service;
+
+    EditText idText;
+    EditText pwText;
+    Button loginBtn;
+    Button signUpBtn;
+    Button findIdBtn;
+    Button findPwBtn;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.common_login);
+        idText = findViewById(R.id.idText);
+        pwText = findViewById(R.id.pwText);
+        loginBtn = findViewById(R.id.loginBtn);
+        signUpBtn = findViewById(R.id.signUpBtn);
+        findIdBtn = findViewById(R.id.findIdBtn);
+        findPwBtn = findViewById(R.id.findPwBtn);
+
+        idText.setOnClickListener(this);
+        pwText.setOnClickListener(this);
+        loginBtn.setOnClickListener(this);
+        signUpBtn.setOnClickListener(this);
+        findIdBtn.setOnClickListener(this);
+        findPwBtn.setOnClickListener(this);
+
+        setRetrofitInit();
+    }
+
+    private void setRetrofitInit() {
+        client = new Retrofit.Builder()
+                .baseUrl(HttpInterface.API_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        service = client.create(HttpInterface.class);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.loginBtn:    // 로그인 버튼
+                login(idText.getText().toString(),pwText.getText().toString());
+
+                break;
+            case R.id.signUpBtn:    // 회원가입 버튼
+                intent = new Intent(getApplicationContext(), JoinChoiceActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.findIdBtn:    // 아이디 찾기 버튼
+                intent = new Intent(getApplicationContext(), FindId1Activity.class);
+                intent.putExtra("name", "koreanFood");
+                startActivity(intent);
+                break;
+            case R.id.findPwBtn:    // 비밀번호 찾기 버튼
+                intent = new Intent(getApplicationContext(), FindPw1Activity.class);
+                intent.putExtra("name", "japanFood");
+                startActivity(intent);
+                break;
+        }
+        //검색
+
     }
 
     //로그인
-    public void login() {
+    public boolean login(String id, String pw) {
+        Call<ResponseBody> call = service.memberLoginInquiry(id, pw);
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                try {
+                    Boolean check = false;
+                    if(!response.body().string().equals("[]")){
+                        check = true;
+                    }
+                    if(check == true) {
+                        SharedPreference.setAttribute(getApplicationContext(),"id",idText.getText().toString());
+                        intent = new Intent(getApplicationContext(),MainActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(getApplicationContext(), "로그인 실패", Toast.LENGTH_SHORT).show();
+                        idText.setText(null);
+                        pwText.setText(null);
+                    }
 
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                System.out.println("응답");
+            }
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), "시스템에 문제가 있습니다.", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        return false;
     }
 }
--- app/src/main/java/kr/co/fos/client/common/MainActivity.java
+++ app/src/main/java/kr/co/fos/client/common/MainActivity.java
@@ -1,32 +1,187 @@
 package kr.co.fos.client.common;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import kr.co.fos.client.R;
 
-public class MainActivity extends AppCompatActivity {
+import kr.co.fos.client.R;
+import kr.co.fos.client.SharedPreference;
+import kr.co.fos.client.bookmark.InquiryActivity;
+import kr.co.fos.client.foodtruck.LocationActivity;
+import kr.co.fos.client.foodtruck.SearchResultActivity;
+import kr.co.fos.client.member.MyInfoActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private SearchView searchView;
+    Button loginBtn;
+    ImageButton koreanFoodBtn;
+    ImageButton japanFoodBtn;
+    ImageButton chinaFoodBtn;
+    ImageButton westernFoodBtn;
+    ImageButton asianFoodBtn;
+    ImageButton snackFoodBtn;
+    ImageButton dessertFoodBtn;
+    Button foodtruckLocationBtn;
+    Button bookmarkBtn;
+    Button infoBtn;
+    Button orderBtn;
+    Button basketBtn;
+    Intent intent;
+    Boolean loginCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.common_main);
+        searchView = findViewById(R.id.searchView);
+        loginBtn = findViewById(R.id.loginBtn);
+        koreanFoodBtn = findViewById(R.id.koreanFoodBtn);
+        japanFoodBtn = findViewById(R.id.japanFoodBtn);
+        chinaFoodBtn = findViewById(R.id.chinaFoodBtn);
+        westernFoodBtn = findViewById(R.id.westernFoodBtn);
+        asianFoodBtn = findViewById(R.id.asianFoodBtn);
+        snackFoodBtn = findViewById(R.id.snackFoodBtn);
+        dessertFoodBtn = findViewById(R.id.dessertFoodBtn);
+        foodtruckLocationBtn = findViewById(R.id.foodtruckLocationBtn);
+        bookmarkBtn = findViewById(R.id.bookmarkBtn);
+        infoBtn = findViewById(R.id.infoBtn);
+        orderBtn = findViewById(R.id.orderBtn);
+        basketBtn = findViewById(R.id.basketBtn);
+
+        loginBtn.setOnClickListener(this);
+        koreanFoodBtn.setOnClickListener(this);
+        japanFoodBtn.setOnClickListener(this);
+        chinaFoodBtn.setOnClickListener(this);
+        westernFoodBtn.setOnClickListener(this);
+        asianFoodBtn.setOnClickListener(this);
+        snackFoodBtn.setOnClickListener(this);
+        dessertFoodBtn.setOnClickListener(this);
+        foodtruckLocationBtn.setOnClickListener(this);
+        bookmarkBtn.setOnClickListener(this);
+        infoBtn.setOnClickListener(this);
+        orderBtn.setOnClickListener(this);
+        basketBtn.setOnClickListener(this);
+        Toast.makeText(MainActivity.this, "검색 처리됨 : " +  SharedPreference.getAttribute(getApplicationContext(),"id"), Toast.LENGTH_SHORT).show();
+        loginCheck = SharedPreference.getAttribute(getApplicationContext(), "id") == null;
+
+        if(!loginCheck) {
+            loginBtn.setText("로그아웃");
+        }
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                intent = new Intent(getApplicationContext(),SearchResultActivity.class);
+                intent.putExtra("name", query);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "검색 처리됨 : " + query, Toast.LENGTH_SHORT).show();
+                return true;
+
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+
+        });
     }
 
-    // 검색
-    public void search() {
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.loginBtn:    // 로그인 버튼
+                if(!(SharedPreference.getAttribute(getApplicationContext(), "id") == null)) {
+                    SharedPreference.removeAttribute(getApplicationContext(),"id");
+                    loginBtn.setText("로그인");
+                } else {
+                    intent = new Intent(getApplicationContext(),LoginActivity.class);
+                    startActivity(intent);
+                }
+                break;
+            case R.id.koreanFoodBtn:    // 한식 버튼
+                intent = new Intent(getApplicationContext(),SearchResultActivity.class);
+                intent.putExtra("name", "koreanFood");
+                startActivity(intent);
+                break;
+            case R.id.japanFoodBtn:    // 일식 버튼
+                intent = new Intent(getApplicationContext(),SearchResultActivity.class);
+                intent.putExtra("name", "japanFood");
+                startActivity(intent);
+                break;
+            case R.id.chinaFoodBtn:    // 중식 버튼
+                intent = new Intent(getApplicationContext(),SearchResultActivity.class);
+                intent.putExtra("name", "chinaFood");
+                startActivity(intent);
+                break;
+            case R.id.westernFoodBtn:    // 양식 버튼
+                intent = new Intent(getApplicationContext(),SearchResultActivity.class);
+                intent.putExtra("name", "westernFood");
+                startActivity(intent);
+                break;
+            case R.id.asianFoodBtn:    // 아시안 버튼
+                intent = new Intent(getApplicationContext(),SearchResultActivity.class);
+                intent.putExtra("name", "asianFood");
+                startActivity(intent);
+                break;
+            case R.id.snackFoodBtn:    // 분식 버튼
+                intent = new Intent(getApplicationContext(),SearchResultActivity.class);
+                intent.putExtra("name", "snackFood");
+                startActivity(intent);
+                break;
+            case R.id.dessertFoodBtn:    // 디저트 버튼
+                intent = new Intent(getApplicationContext(),SearchResultActivity.class);
+                intent.putExtra("name", "dessertFood");
+                startActivity(intent);
+                break;
+            case R.id.foodtruckLocationBtn:    // 내 주변 푸드트럭 검색 버튼
+                intent = new Intent(getApplicationContext(), LocationActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.bookmarkBtn:    // 즐겨찾기 버튼
+                intent = new Intent(getApplicationContext(),kr.co.fos.client.bookmark.InquiryActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.infoBtn:    // 내 정보 버튼
+                intent = new Intent(getApplicationContext(), MyInfoActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.orderBtn:    // 주문 버튼
+                intent = new Intent(getApplicationContext(), kr.co.fos.client.order.InquiryActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.basketBtn:    // 장바구니 버튼
+                intent = new Intent(getApplicationContext(), kr.co.fos.client.basket.InquiryActivity.class);
+                startActivity(intent);
+                break;
+
+        }
+        //검색
 
     }
+
 
     // 로그아웃
     public void logout() {
 
     }
 
-    // 항목 별 검색
-    public void categorySearch() {
 
-    }
 
 }
